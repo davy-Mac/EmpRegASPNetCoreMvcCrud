4b945e6ecfcf24f2deadb07abf1dd2ab503472a1
David Maceda <pankoko@hotmail.com>
2019-01-30 20:54:56 +0800

    Initial to End

--- EmpRegASPNetCoreMvcCrud/Startup.cs
+++ EmpRegASPNetCoreMvcCrud/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmpRegASPNetCoreMvcCrud.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,6 +34,9 @@ namespace EmpRegASPNetCoreMvcCrud
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<EmployeeContext>(options=>
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +58,7 @@ namespace EmpRegASPNetCoreMvcCrud
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Employee}/{action=Index}/{id?}");
             });
         }
     }
